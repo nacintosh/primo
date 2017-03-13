47975aa1223dfe9fecd30f4f98c1a27a0885cd94
Kazuhito NAKAMURA <kazuhito.nakamura@users.noreply.github.com>
2017-03-14 02:33:43 +0900

    get videoid from slack channel

--- public/functions/listen/index.js
+++ public/functions/listen/index.js
@@ -14,8 +14,32 @@ const query = datastore.createQuery('YouTube').order('created', {
   descending: true
 });
 
+var WebClient = require('@slack/client').WebClient;
+var token = process.env.SLACK_API_TOKEN || '';
+var web = new WebClient(token);
+
 exports.listen = function listen(req, res) {
-  datastore.runQuery(query, (err, entities) => {
-    ssr.default(req, res, entities);
+  web.channels.history('C421S7TCH', {'count' : 1000}, function(err, info) {
+     if (err) {
+       console.log(err);
+       return;
+     }
+     var entities = [];
+     for(var i in info.messages) {
+       var matches = info.messages[i].text.match(/https:\/\/www\.youtube\.com\/watch\?v=[A-Za-z0-9_-]+/g);
+       if (!matches) {
+         continue;
+       }
+       for (var j in matches) {
+         var videoid = matches[j].match(/v=[A-Za-z0-9_-]+/g)[0].substr(2); // v=
+         entities.push({ 'videoid' : videoid } );
+       }
+     }
+     ssr.default(req, res, entities);
   });
+
+//  datastore.runQuery(query, (err, entities) => {
+//    console.log(entities);
+//    ssr.default(req, res, entities);
+//  });
 };
